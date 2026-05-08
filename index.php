f7a6e1347fa37b44e5dd784d2c295e8791050716
fabianpid <fabianpid34@gmail.com>
2026-05-08 12:46:47 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -136,6 +136,23 @@ if (!$resultado) {
                 <label>opinion:</label>
                 <input type="text" id="opinion" name="opinion" placeholder="Tu opinion aqui" required>
                 <button type="submit" name="enviar" class="btn-submit">Enviar Consulta</button>
+                <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+if(isset($_POST['enviar'])){
+    // 1. Recibimos datos
+    $opinion = $_POST['opinion'];
+    $sql = "INSERT INTO parecio (opinion) VALUES ('$opinion')";
+    $query = mysqli_query($conexion, $sql);
+
+    if($query){
+        echo "¡Éxito! Usuario insertado.";
+    } else {
+        // ESTA LÍNEA ES CLAVE: Te dirá qué tiene de malo tu base de datos
+        echo "Error de SQL: " . mysqli_error($conexion);
+    }
+} 
+?>
             </form>
         </section>
 
