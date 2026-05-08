8875214bec4c6bab169b6d439ccd831110ea6650
fabianpid <fabianpid34@gmail.com>
2026-05-07 20:32:55 -0600

    Update index.php

--- index.php
+++ index.php
@@ -129,7 +129,7 @@ if (!$resultado) {
             <h3>¿Te fue de utilidad la pagina? dejanos tu opinion </h3>
             <p>Envíanos tu opinion.</p>
             <form action="conect.php" method="POST">
-                <label for="opinion">Nombre Completo:</label>
+                <label for="opinion">opinion:</label>
                 <input type="text" id="opinion" name="opinion" placeholder="Tu opinion aqui" required>
                 <button type="submit">Enviar Consulta</button>
             </form>
